9f870055bd20aba05af57e025cd597c626fac6be
Lisa Armstrong <lisa@workingwebsites.ca>
2022-05-25 10:12:19 -0700

    Home is a class

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,17 @@
-const Home = () => {
-  return <h1>Home</h1>;
-};
+import React from "react";
+import imgXs from "../assets/images/360px.png";
+import imgSm from "../assets/images/576px.png";
+import imgMd from "../assets/images/768px.png";
+import imgLg from "../assets/images/992px.png";
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Home</h1>
+        <img src={imgLg} alt="992px" width="200" />
+      </div>
+    );
+  }
+}
 export default Home;
