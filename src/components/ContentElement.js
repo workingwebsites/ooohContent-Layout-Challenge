58da155809a649ab95480ea26c8d7bb3d8276bec
Lisa Armstrong <lisa@workingwebsites.ca>
2022-05-25 18:59:32 -0700

    Clean up

--- src/components/ContentElement.js
+++ src/components/ContentElement.js
@@ -5,28 +5,24 @@ export default class ContentElement extends Component {
     const { img, heading, copy, btnCopy, authorImg, authorName, authorEmail } = this.props;
 
     return (
-      <div className="content-element col-12 col-md-12 col-lg-4 ">
-        <div className="test">
-          <div className="testBox">Box One</div>
-          <div className="testBox">Box Two</div>
-          <div className="testBox">Box Three</div>
-        </div>
+      <div className="content-element col-12 col-md-12 col-lg-4 mb-4">
+        <div className="flex-container">
+          {/* Author */}
+          <div className="content-element__author flex-container__div">
+            <img className="content-element__author-img img-fluid" src={authorImg} alt={authorName} />
+            <h3 className="content-element__author-name">{authorName}</h3>
+            <a href={`mailto:${authorEmail}`} className="content-element__author-email">
+              {authorEmail}
+            </a>
+          </div>
 
-        {/* Author */}
-        <div className="content-element__author">
-          <img className="content-element__author-img img-fluid" src={authorImg} alt={authorName} />
-          <h3 className="content-element__author-name">{authorName}</h3>
-          <a href={`mailto:${authorEmail}`} className="content-element__author-email">
-            {authorEmail}
-          </a>
-        </div>
-
-        {/* Book */}
-        <div className="content-element__content">
-          <img className="content-element__content-img img-fluid" src={img} alt={heading} />
-          <h2 className="content-element__content-heading">{heading}</h2>
-          <div className="content-element__content-copy">{copy}</div>
-          <button className="content-element__content-btn btn btn-primary">{btnCopy}</button>
+          {/* Book */}
+          <div className="content-element__content flex-container__div">
+            <img className="content-element__content-img img-fluid" src={img} alt={heading} />
+            <h2 className="content-element__content-heading">{heading}</h2>
+            <div className="content-element__content-copy">{copy}</div>
+            <button className="content-element__content-btn btn btn-primary">{btnCopy}</button>
+          </div>
         </div>
       </div>
     );
